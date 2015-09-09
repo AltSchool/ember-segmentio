cfda2262c424a80beb93e628f2a5f7d1a7763a64
Tristan Goffman <tristan@altschool.com>
2015-09-09 14:18:20 -0700

    Add snippet fetching to HEAD

--- README.md
+++ README.md
@@ -1,25 +1,4 @@
-# Ember-segmentio
+Ember Segment IO integration
 
-This README outlines the details of collaborating on this Ember addon.
+Set SEGMENTIO_KEY on your parent application or addon as environment variable.
 
-## Installation
-
-* `git clone` this repository
-* `npm install`
-* `bower install`
-
-## Running
-
-* `ember server`
-* Visit your app at http://localhost:4200.
-
-## Running Tests
-
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
-
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
+function readSnippet() {
+  try {
+    return fs.readFileSync(path.join(process.cwd(), 'vendor/snippet-core.min.js'), {
+      encoding: 'UTF-8'
+    });
+  } catch(error) {
+    if (error.code === 'ENOENT') {
+      return '';
+    } else {
+      throw error;
+    }
+  }
+}
+
+
 module.exports = {
-  name: 'ember-segmentio'
+  name: 'ember-segmentio',
+
+  contentFor: function(type, config) {
+    var content = '';
+    var coreSnippet = readSnippet();
+    var enabled = config.SEGMENTIO_KEY && coreSnippet;
+
+    // Warn when no snippet found in development regardless of whether new relic
+    // is enabled.
+
+    if (!config.SEGMENTIO_KEY) {
+      console.warn('ember-segmentio: Not enabled missing key "SEGMENTIO_KEY" in parent application/addon.');
+    }
+
+    if (!coreSnippet) {
+      console.warn('ember-segmentio: Error while reading snippet.');
+    }
+
+    if (enabled && type === 'head') {
+      content = [
+        "!function(){",
+        coreSnippet,
+        "analytics.load(", config.SEGMENTIO_KEY, ");",
+        "analytics.page()",
+        "}()"
+      ].join("");
+    }
+    return content;
+  }
+
 };
