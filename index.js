ac3830bde4f9eb2345178dc12fb72fe8c5ed9329
Tristan Goffman <tristan@altschool.com>
2015-09-09 14:18:20 -0700

    Add snippet fetching to HEAD

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
+function readSnippet() {
+  try {
+    // NOTE: expecting addon as a node_module on parent addon or in an Ember CLI application.
+    return fs.readFileSync(path.join(process.cwd(), 'node_modules/ember-segmentio/vendor/snippet-core.min.js'), {
+      encoding: 'UTF-8'
+    });
+  } catch(error) {
+    if (error.code === 'ENOENT') {
+      return '';
+    } else {
+      throw error;
+    }
+  }
+}
+
 module.exports = {
-  name: 'ember-segmentio'
+  name: 'ember-segmentio',
+
+  contentFor: function(type, config) {
+    var content = '';
+    var coreSnippet = readSnippet();
+    var enabled = config.SEGMENTIO_KEY && coreSnippet;
+
+    if (!config.SEGMENTIO_KEY) {
+      console.warn('ember-segmentio: Not enabled missing key "SEGMENTIO_KEY" in parent application/addon.');
+    }
+
+    if (!coreSnippet) {
+      console.warn('ember-segmentio: Error while reading snippet.');
+    }
+
+    if (enabled && type === 'head') {
+      content = [
+        '<script type="text/javascript">',
+        '!function(){',
+        coreSnippet,
+        'analytics.load("', config.SEGMENTIO_KEY, '");',
+        'analytics.page()',
+        '}();',
+        '</script>'
+      ].join('');
+    }
+    return content;
+  }
+
 };
